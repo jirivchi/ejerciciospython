da89fc45633dd06a5fe24bb8b1f91f7d63c6a726
jirivchi <jirivchi@alu.upo.es>
2022-10-02 18:50:29 +0200

    Initial commit

---
+++ 400ejercicios.py
@@ -0,0 +1,11 @@
+#ejercicio 277
+#lista = int(input('escriba lista: '))
+
+lista = [1,3,6,10]
+newlist = []
+for elemento in range(1,len(lista)):
+    i=lista[elemento] - lista[elemento-1]
+    print(i)
+    newlist.append(i)
+
+print(sum(newlist))
